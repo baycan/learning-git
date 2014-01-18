d82c95aae6c1f11a5197a419dfae8f0066476b5b
baycan <baycan.vural@gmail.com>
2014-01-18 13:14:18 +0200

    added function for open serial port

--- git-project.c
+++ git-project.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 #include<conio.h>
 
+void open_serial(void);
+
 int main()
 {
 int i=0;
@@ -9,5 +11,15 @@ for( i=0; i<10 ; i++)
 printf("%d",i);
 
 printf("ilk git deneme");
+
+open_serial();
+
+
 return 0;
+}
+
+void open_serial()
+{
+printf("serial port opening..");
+
 }
